a797859a8e4e357df1cd2e645edd1f1b118797ab
kimbring2 <kimbring2@gmail.com>
2026-03-06 12:13:04 +0900

    README update

--- source/ses_v2_lss/ses_v2_lss/tasks/manager_based/ses_v2_lss/__init__.py
+++ source/ses_v2_lss/ses_v2_lss/tasks/manager_based/ses_v2_lss/__init__.py
@@ -11,7 +11,7 @@ from . import agents
 # Register Gym environments.
 ##
 gym.register(
-    id="Template-Ses-V2-Lss-v0",
+    id="SES-V2-LSS-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
@@ -22,7 +22,7 @@ gym.register(
 
 
 gym.register(
-    id="Template-Ses-V2-Lss-Play-v0",
+    id="SES-V2-LSS-Play-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
